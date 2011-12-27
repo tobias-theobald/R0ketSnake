ef47133bf49b54d72920f1014809d328eda71617
dodo <dodo@dodo-TravelMate-5720.(none)>
2011-12-28 00:33:42 +0100

    gitignore ~ and some snake stuff

--- snake.c
+++ snake.c
@@ -1,6 +1,10 @@
 #define SCREEN_WIDTH  96
 #define SCREEN_HEIGHT 67
 #define SCREEN_SIZE ( SCREEN_WIDTH * SCREEN_HEIGHT )
+#define BLOCK_SIZE 4
+#define GAME_WIDTH ( SCREEN_WIDTH / BLOCK_SIZE )
+#define GAME_HEIGHT ( SCREEN_HEIGHT / BLOCK_SIZE )
+#define GAME_SIZE ( GAME_WIDTH * GAME_HEIGHT ) 
 
 #define INITIAL_LENGTH 4
 
@@ -10,12 +14,22 @@ typedef struct {
 } point;
 
 typedef struct {
-	int8_t start;
+	point startpoint;
+	size_t starthn; // index of the half nibble (2 bits) where we start
 	int8_t length;
-	point[SCREEN_SIZE] data; // TODO: optimize to a power of 2
+	int8_t[GAME_SIZE / 4] data; // TODO: optimize to a power of 2
 } vringpbuf; // variable (but limited) size ring buffer for points
 
+int8_t shiftBuf (vringpbuf *buf, int8_t nextDir);
+int8_t growBuf (vringpbuf *buf, int8_t nextDir);
+
+
 vringpbuf snake;
+#define DIRECTION_RIGHT 0
+#define DIRECTION_UP 1
+#define DIRECTION_LEFT 2
+#define DIRECTION_DOWN 3
+int8_t direction;
 
 int8_t i;
 
@@ -23,19 +37,55 @@ void initSnake (vringpbuf *);
 
 void game (void) {
 	lcdClear();
-	
+	initSnake ();
 	while (1) {
 		
-		
+		int key = getInputRaw();
+		switch (key) {
+			case BTN_ENTER:
+				// exit
+				return 0;
+			case BTN_RIGHT:
+				if (direction != DIRECTION_LEFT)
+					direction = DIRECTION_RIGHT;
+			break;
+			case BTN_UP:
+				if (direction != DIRECTION_DOWN)
+					direction = DIRECTION_UP;
+			break;
+			case BTN_LEFT:
+				if (direction != DIRECTION_RIGHT)
+					direction = DIRECTION_LEFT;
+			break;
+			case BTN_DOWN:
+				if (direction != DIRECTION_UP)
+					direction = DIRECTION_DOWN;
+			break;
+			default:
+				// strange things happen!
+				return 1;
+		}
 	}
 }
 
-void initSnake (vringpbuf *s) {
-	s->start=0;
-	s->length=INITIAL_LENGTH;
-	for (i=0; i<INITIAL_LENGTH; i++) {
-		s->data[i].x=i;
-		s->data[i].y=0;
-	}
+int8_t getHalfNibble (int8_t *data, int index) {
+	int8_t ret = data[index/4];
+	return (ret >> (index % 4)) & 3;
+}
+
+void setHalfNibble (int8_t *data, int index, int8_t value) {
+	//TODO
+}
+
+int8_t shiftBuf (vringpbuf *buf, int8_t nextDir) {
+	//TODO
+}
+
+int8_t growBuf (vringpbuf *buf, int8_t nextDir) {
+	//TODO
+}
+
+void initSnake (void) {
+	//TODO
 }
 
